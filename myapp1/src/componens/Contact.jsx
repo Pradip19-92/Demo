837323f16c55b1f13377631ed99cedf37091e692
unknown <pradipvaghela56789@gmail.com>
2025-02-25 13:56:07 +0530

    changed color

--- myapp1/src/componens/Contact.jsx
+++ myapp1/src/componens/Contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
       <h1 style={{color : myColor}}>Welcome</h1>
       <h1>{username}</h1>
       <button className="btn btn-success ms-2" onClick={handleClick}>Click Me</button>
-      <button className="btn btn-danger ms-2" onClick={() => {setMyColor('red')}}>Click</button>
+      <button className="btn btn-danger ms-2" onClick={() => {setMyColor('green')}}>Click</button>
       <button className="btn btn-primary ms-2" onClick={() => {setMyColor('blue')}}>Click</button>
       
 
